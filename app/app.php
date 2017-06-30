3618380779529d7c010733e97ffbafcd7d8deade
Steve Spitz <steve@spitz.com>
2017-06-30 14:21:05 -0700

    Add corrections from final recheck of code appearance

--- app/app.php
+++ app/app.php
@@ -13,17 +13,21 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"
     ));
+
     $app->get("/", function() use ($app) {
         return $app["twig"]->render("landing.html.twig");
     });
+
     $app->get("/home", function() use ($app) {
         return $app["twig"]->render("home.html.twig", array("contacts" => Contact::getAll()));
     });
+
     $app->post("/create_contact", function() use ($app) {
         $contact = new Contact($_POST['name'], $_POST['phone_number'], $_POST['address']);
         $contact->save();
         return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
     });
+
     $app->post("/delete_contacts", function() use ($app) {
         Contact::deleteAll();
         return $app['twig']->render('delete_contacts.html.twig');
--- src/Src.php
+++ src/Src.php
@@ -4,42 +4,41 @@ class Contact
       private $name;
       private $phone_number;
       private $address;
-      
+
       function __construct($name, $phone_number, $address)
         {
             $this->name = $name;
             $this->phone_number = $phone_number;
             $this->address = $address;
         }
-
-    function getName() {
-        return $this->name;
-    }
-    function getPhoneNumber() {
-        return $this->phone_number;
-    }
-    function getAddress() {
-        return $this->address;
-    }
-    function setName($new_name) {
-        $this->name = (string) $new_name;
-    }
-    function setPhoneNumber($new_number) {
-        $this->phone_number = $new_number;
-    }
-    function setAddress($new_address) {
-        $this->address = $new_address;
-    }
-    static function getAll() {
-        return $_SESSION['list_of_contacts'];
-    }
-    function save()
-    {
-        array_push($_SESSION['list_of_contacts'], $this);
-    }
-    static function deleteAll()
-    {
-        return $_SESSION['list_of_contacts'] = array();
-    }
+        function getName() {
+            return $this->name;
+        }
+        function getPhoneNumber() {
+            return $this->phone_number;
+        }
+        function getAddress() {
+            return $this->address;
+        }
+        function setName($new_name) {
+            $this->name = (string) $new_name;
+        }
+        function setPhoneNumber($new_number) {
+            $this->phone_number = $new_number;
+        }
+        function setAddress($new_address) {
+            $this->address = $new_address;
+        }
+        static function getAll() {
+            return $_SESSION['list_of_contacts'];
+        }
+        function save()
+        {
+            array_push($_SESSION['list_of_contacts'], $this);
+        }
+        static function deleteAll()
+        {
+            return $_SESSION['list_of_contacts'] = array();
+        }
     }
 ?>

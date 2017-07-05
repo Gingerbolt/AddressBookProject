32a283b538981ee356ce2d6374e14146802b8a9a
Steve Spitz <steve@spitz.com>
2017-07-04 17:42:42 -0700

    Convert remaining html variables to upper camel case

--- app/app.php
+++ app/app.php
@@ -22,15 +22,15 @@
         return $app["twig"]->render("home.html.twig", array("contacts" => Contact::getAll()));
     });
 
-    $app->post("/create_contact", function() use ($app) {
-        $contact = new Contact($_POST['name'], $_POST['phone_number'], $_POST['address']);
+    $app->post("/createContact", function() use ($app) {
+        $contact = new Contact($_POST['name'], $_POST['phoneNumber'], $_POST['address']);
         $contact->save();
-        return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
+        return $app['twig']->render('createContact.html.twig', array('newcontact' => $contact));
     });
 
-    $app->post("/delete_contacts", function() use ($app) {
+    $app->post("/deleteContacts", function() use ($app) {
         Contact::deleteAll();
-        return $app['twig']->render('delete_contacts.html.twig');
+        return $app['twig']->render('deleteContacts.html.twig');
     });
 
 return $app;
